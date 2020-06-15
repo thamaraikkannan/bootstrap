8d49f419850dc242b5b3a814469bc054b418f85a
kannan <thamaraikannan995@gmail.com>
2020-06-15 22:50:45 +0530

    port value

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-const path = require('path')
 const PORT = process.env.PORT || 5000
-
-express()
-  
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
