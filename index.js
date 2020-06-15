45d4d983833e3694a74608a171c0e8bc12e45579
kannan <thamaraikannan995@gmail.com>
2020-06-15 22:46:12 +0530

    removed ejs

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .set('view engine', 'ejs')
+  
   .get('/', (req, res) => res.render('index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
